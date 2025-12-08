eshenayo/valkey-search

commit 43b7e3cc6dd5d7a53b6139d31f9c4660f3a5ab3f

Add support for checking ACL key prefix permissions using the new Valkey module API. (#479)
* Add support for checking ACL key prefix permissions in Valkey module

This change introduces a new function `AclValkeyCheckPermissions` that leverages Valkey's native ACL infrastructure to verify user permissions for key prefixes. It uses the new `ValkeyModule_ACLCheckKeyPrefixPermissions` API when available, falling back to the legacy `Call(ACL,...)` approach for older Valkey versions. Additionally, the build script is updated to properly detect and use the configured build tool (make or ninja) for CMake integration.

Updated `.clang-format` for `PointerAlignment: Right` (with `clang-format v21` pointer is aligned **LEFT** with Google's style).

* ACL permission checking: Added fast-path permission validation using ValkeyModule_ACLCheckKeyPrefixPermissions
* Build system: Improved build tool detection logic in build.sh
* Managed pointers: Added UniqueValkeyModuleUser for automatic ValkeyModuleUser cleanup
* Header reorganization: Fixed include paths in commands.h

** Generated by CodeLite. **

Signed-off-by: Eran Ifrah <eifrah@amazon.com>

* Addressed PR comments.

Signed-off-by: Eran Ifrah <eifrah@amazon.com>

---------

Signed-off-by: Eran Ifrah <eifrah@amazon.com>

src/acl.cc
@@ -7,6 +7,7 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/match.h"
 #include "absl/strings/str_split.h"
+#include "src/commands/commands.h"
 #include "vmsdk/src/managed_pointers.h"
 #include "vmsdk/src/status/status_macros.h"
 #include "vmsdk/src/valkey_module_api/valkey_module.h"
@@ -187,6 +188,15 @@ absl::string_view CallReplyStringToStringView(ValkeyModuleCallReply *r) {
   return {r_c_str, l};
 }
 
+absl::string_view KeyAccessToStringView(acl::KeyAccess ka) {
+  switch (ka) {
+    case acl::KeyAccess::kWrite:
+      return kWriteCategory;
+    case acl::KeyAccess::kRead:
+      return kReadCategory;
+  }
+}
+
 /*
 The ACL rules provided by the Valkey Server ACL GETUSER command, are a list of
 rules contain commands and key patterns, which describe if the commands can run
@@ -194,7 +204,6 @@ against the key patterns. We only need the key patterns that the corresponding
 commands matches our module commands.
 */
 std::vector<absl::string_view> GetKeysWithAllowedCommands(
-    const absl::flat_hash_set<absl::string_view> &module_allowed_cmds,
     const std::vector<acl::ValkeyAclGetUserReplyView> &acl_views) {
   std::vector<absl::string_view> keys;
   for (const auto &acl_view : acl_views) {
@@ -278,6 +287,10 @@ void GetAclViewFromCallReplyImpl(
   }
 }
 
+bool IsKeyPrefixCheckSupported() {
+  return ValkeyModule_ACLCheckKeyPrefixPermissions != nullptr;
+}
+
 }  // namespace
 
 absl::StatusOr<std::vector<acl::ValkeyAclGetUserReplyView>>
@@ -292,26 +305,73 @@ GetAclViewFromCallReply(ValkeyModuleCallReply *reply) {
   return acl_views;
 }
 
-absl::Status AclPrefixCheck(
-    ValkeyModuleCtx *ctx,
-    const absl::flat_hash_set<absl::string_view> &module_allowed_cmds,
+/**
+ * @brief Checks if the current user has the required permissions for all
+ * specified key prefixes.
+ *
+ * This function verifies that the current Valkey module user has the specified
+ * access rights (read, write, or both) for every key prefix in the provided
+ * list. It returns an error if any prefix check fails or if key prefix
+ * permissions are not supported.
+ *
+ * @param ctx Pointer to the Valkey module context.
+ * @param access The type of access to check (read, write, or both).
+ * @param module_prefixes Vector of key prefixes to check permissions for.
+ *
+ * @return absl::Status Returns `absl::OkStatus()` if all checks pass. Returns
+ *         `absl::UnimplementedError` if key prefix checks are not supported by
+ * this Valkey version. Returns `absl::PermissionDeniedError` if the user lacks
+ * permissions for any prefix.
+ */
+absl::Status AclValkeyCheckPermissions(
+    ValkeyModuleCtx *ctx, acl::KeyAccess access,
     const std::vector<std::string> &module_prefixes) {
+  if (!IsKeyPrefixCheckSupported()) {
+    return absl::UnimplementedError(
+        "ValkeyModule_ACLCheckKeyPrefixPermissions is not supported for this "
+        "version of Valkey");
+  }
+
+  auto flags = static_cast<unsigned int>(access);
+  auto valkey_username =
+      vmsdk::UniqueValkeyString(ValkeyModule_GetCurrentUserName(ctx));
+
+  auto module_user = vmsdk::UniqueValkeyModuleUser(
+      ValkeyModule_GetModuleUserFromUserName(valkey_username.get()));
+
+  // We allow the user to execute command only if the user has permissions for
+  // ALL of the prefixes of the index
+  for (const auto &prefix : module_prefixes) {
+    if (ValkeyModule_ACLCheckKeyPrefixPermissions(
+            module_user.get(), prefix.c_str(), prefix.length(), flags) !=
+        VALKEYMODULE_OK) {
+      return absl::PermissionDeniedError(
+          "The user does not have permission to access the key prefix");
+    }
+  }
+  return absl::OkStatus();
+}
+
+absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx,
+                            [[maybe_unused]] absl::string_view access,
+                            const std::vector<std::string> &module_prefixes) {
   if (!vmsdk::IsRealUserClient(ctx)) {
     return absl::OkStatus();
   }
+
   auto username =
       vmsdk::UniqueValkeyString(ValkeyModule_GetCurrentUserName(ctx));
   auto reply = vmsdk::UniquePtrValkeyCallReply(
       ValkeyModule_Call(ctx, "ACL", "cs3", "GETUSER", username.get()));
   VMSDK_ASSIGN_OR_RETURN(auto acl_views, GetAclViewFromCallReply(reply.get()));
 
-  auto acl_keys = GetKeysWithAllowedCommands(module_allowed_cmds, acl_views);
+  auto acl_keys = GetKeysWithAllowedCommands(acl_views);
 
   // If no index prefixes are given from the module, it should have permission
   // to access ALL keys.
   if (module_prefixes.empty() && !IsPrefixAllowed("", acl_keys)) {
     return absl::PermissionDeniedError(
-        "The user doesn't have a permission to execute a command");
+        "The user does not have permission to access the key prefix");
   }
 
   // We allow the user to execute command only if the user has permissions for
@@ -319,22 +379,27 @@ absl::Status AclPrefixCheck(
   for (const auto &module_prefix : module_prefixes) {
     if (!IsPrefixAllowed(module_prefix, acl_keys)) {
       return absl::PermissionDeniedError(
-          "The user doesn't have a permission to execute a command");
+          "The user does not have permission to access the key prefix");
     }
   }
   return absl::OkStatus();
 }
 
-absl::Status AclPrefixCheck(
-    ValkeyModuleCtx *ctx,
-    const absl::flat_hash_set<absl::string_view> &module_allowed_cmds,
-    const data_model::IndexSchema &index_schema_proto) {
+absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
+                            const data_model::IndexSchema &index_schema_proto) {
   std::vector<std::string> module_prefixes;
   module_prefixes.reserve(index_schema_proto.subscribed_key_prefixes_size());
   for (const auto &prefix : index_schema_proto.subscribed_key_prefixes()) {
     module_prefixes.emplace_back(prefix);
   }
-  return AclPrefixCheck(ctx, module_allowed_cmds, module_prefixes);
+  return AclPrefixCheck(ctx, KeyAccessToStringView(access), module_prefixes);
 }
 
+absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
+                            const std::vector<std::string> &module_prefixes) {
+  if (IsKeyPrefixCheckSupported()) {
+    return AclValkeyCheckPermissions(ctx, access, module_prefixes);
+  }
+  return AclPrefixCheck(ctx, KeyAccessToStringView(access), module_prefixes);
+}
 }  // namespace valkey_search

.clang-format
@@ -1,3 +1,4 @@
 BasedOnStyle: Google
 IndentWidth: 2
 ColumnLimit: 80
+PointerAlignment: Right

build.sh
@@ -261,39 +261,44 @@ function check_tools() {
     done
 
     os_name=$(uname -s)
-    if [[ "${BUILD_TOOL}" == "make" ]]; then
-        NINJA_TOOL="make"
+    if [[ "${BUILD_TOOL}" =~ make ]]; then
+        BUILDER_TOOL="make"
     elif [[ "${os_name}" == "Darwin" ]]; then
         # ninja is can be installed via "brew"
-        NINJA_TOOL="ninja"
+        BUILDER_TOOL="ninja"
     else
         # Check for ninja. On RedHat based Linux, it is called ninja-build, while on Debian based Linux, it is simply ninja
         # Ubuntu / Mint et al will report "ID_LIKE=debian"
         local debian_output=$(cat /etc/*-release | grep -i debian | wc -l)
         if [ ${debian_output} -gt 0 ]; then
-            NINJA_TOOL="ninja"
+            BUILDER_TOOL="ninja"
         else
-            NINJA_TOOL="ninja-build"
+            BUILDER_TOOL="ninja-build"
         fi
     fi
-    check_tool ${NINJA_TOOL}
+    check_tool ${BUILD_TOOL}
 }
 
 # If any of the CMake files is newer than our "build.ninja" file, force "cmake" before building
 function is_configure_required() {
-    local ninja_build_file=${BUILD_DIR}/build.ninja
+    if [[ "${BUILD_TOOL}" == "ninja" ]]; then
+      local top_level_build_file=${BUILD_DIR}/build.ninja
+    else
+      local top_level_build_file=${BUILD_DIR}/Makefile
+    fi
+
     if [[ "${RUN_CMAKE}" == "yes" ]]; then
         # User asked for configure
         echo "yes"
         return
     fi
 
-    if [ ! -f "${ninja_build_file}" ] || [ ! -f "${BUILD_DIR}/CMakeCache.txt" ]; then
-        # No ninja build file
+    if [ ! -f "${top_level_build_file}" ] || [ ! -f "${BUILD_DIR}/CMakeCache.txt" ]; then
         echo "yes"
         return
     fi
-    local build_file_lastmodified=$(get_file_last_modified "${ninja_build_file}")
+
+    local build_file_lastmodified=$(get_file_last_modified "${top_level_build_file}")
     local IFS=$'\n'
     local cmake_files=$(find "${ROOT_DIR}" -name "CMakeLists.txt" -o -name "*.cmake" | grep -v ".build-release" | grep -v ".build-debug")
     for cmake_file in $cmake_files; do
@@ -333,6 +338,7 @@ TESTS_DIR=${BUILD_DIR}/tests
 TEST_OUTPUT_FILE=${BUILD_DIR}/tests.out
 
 printf "Checking if configure is required..."
+
 FORCE_CMAKE=$(is_configure_required)
 printf "${GREEN}${FORCE_CMAKE}${RESET}\n"
 check_tools

integration/test_valkey_search_acl.py
@@ -177,7 +177,7 @@ class TestCommandsACLs(ValkeySearchTestCaseBase):
         search_vector = struct.pack("<3f", *[1.0, 2.0, 3.0])
         with pytest.raises(
             ResponseError,
-            match="The user doesn't have a permission to execute a command",
+            match="The user does not have permission to access the key prefix",
         ):
             client.execute_command(
                 "FT.SEARCH",

src/acl.h
@@ -9,6 +9,11 @@
 namespace valkey_search {
 namespace acl {
 
+enum class KeyAccess : unsigned int {
+  kRead = VALKEYMODULE_CMD_KEY_ACCESS,
+  kWrite = VALKEYMODULE_CMD_KEY_INSERT,
+};
+
 struct ValkeyAclGetUserReplyView {
   absl::string_view cmds;
   absl::string_view keys;
@@ -28,15 +33,14 @@ Check if
 `module_prefixes`,
     * according to the ACL rules defined in the server.
 */
-absl::Status AclPrefixCheck(
-    ValkeyModuleCtx *ctx,
-    const absl::flat_hash_set<absl::string_view> &module_allowed_cmds,
-    const std::vector<std::string> &module_prefixes);
+absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, absl::string_view access,
+                            const std::vector<std::string> &module_prefixes);
 
-absl::Status AclPrefixCheck(
-    ValkeyModuleCtx *ctx,
-    const absl::flat_hash_set<absl::string_view> &module_allowed_cmds,
-    const data_model::IndexSchema &index_schema_proto);
+absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
+                            const data_model::IndexSchema &index_schema_proto);
+
+absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
+                            const std::vector<std::string> &module_prefixes);
 
 }  // namespace valkey_search
 #endif  // VALKEYSEARCH_SRC_COMMANDS_ACL_H_

src/commands/commands.cc
@@ -86,10 +86,9 @@ absl::Status QueryCommand::Execute(ValkeyModuleCtx *ctx,
     parameters->parse_vars.ClearAtEndOfParse();
     parameters->cancellation_token =
         cancel::Make(parameters->timeout_ms, nullptr);
-    static const auto permissions =
-        PrefixACLPermissions(kSearchCmdPermissions, kSearchCommand);
-    VMSDK_RETURN_IF_ERROR(AclPrefixCheck(
-        ctx, permissions, parameters->index_schema->GetKeyPrefixes()));
+    VMSDK_RETURN_IF_ERROR(
+        AclPrefixCheck(ctx, acl::KeyAccess::kRead,
+                       parameters->index_schema->GetKeyPrefixes()));
 
     parameters->index_schema->ProcessMultiQueue();
 

src/commands/commands.h
@@ -11,8 +11,8 @@
 #include "absl/container/flat_hash_set.h"
 #include "absl/status/status.h"
 #include "absl/strings/string_view.h"
-#include "command_parser.h"
 #include "src/query/search.h"
+#include "vmsdk/src/command_parser.h"
 #include "vmsdk/src/valkey_module_api/valkey_module.h"
 
 namespace valkey_search {

src/commands/ft_create.cc
@@ -51,9 +51,8 @@ absl::Status FTCreateCmd(ValkeyModuleCtx *ctx, ValkeyModuleString **argv,
   VMSDK_ASSIGN_OR_RETURN(auto index_schema_proto,
                          ParseFTCreateArgs(ctx, argv + 1, argc - 1));
   index_schema_proto.set_db_num(ValkeyModule_GetSelectedDb(ctx));
-  static const auto permissions =
-      PrefixACLPermissions(kCreateCmdPermissions, kCreateCommand);
-  VMSDK_RETURN_IF_ERROR(AclPrefixCheck(ctx, permissions, index_schema_proto));
+  VMSDK_RETURN_IF_ERROR(
+      AclPrefixCheck(ctx, acl::KeyAccess::kWrite, index_schema_proto));
   VMSDK_ASSIGN_OR_RETURN(
       auto new_entry_fingerprint_version,
       SchemaManager::Instance().CreateIndexSchema(ctx, index_schema_proto));

commit 5735801cc2bfedd9ee49dcc7067a4965605d0a6e

Fix replica crash by using legacy ACL path during replication (#510)
* Fix replica crash by using legacy ACL path during replication

Modify AclPrefixCheck to use the new fast ACL API only for real user clients.
During replication context, fall back to the legacy ACL path which has proper
safety checks and prevents segmentation fault in VM_GetModuleUserFromUserName.

This approach:
- Preserves fast ACL checking for real users when new API is available
- Uses safe legacy path during replication/AOF/internal operations
- Prevents crashes while maintaining performance benefits

Fixes crash introduced in commit 43b7e3c when ACL checking was added.

* DCO Remediation Commit for Elias Tamraz <tamrazelias@gmail.com>

I, Elias Tamraz <tamrazelias@gmail.com>, hereby add my Signed-off-by to this commit: 0384de5ba783370b702e3f7abef2982c735f2220

Address code review feedback:
- Add early return for non-real users to avoid unnecessary work
- Replace conditional check with assertion for better error handling

Co-authored-by: yairgott
Signed-off-by: Elias Tamraz <tamrazelias@gmail.com>

---------

Signed-off-by: Elias Tamraz <tamrazelias@gmail.com>

src/acl.cc
@@ -328,11 +328,7 @@ GetAclViewFromCallReply(ValkeyModuleCallReply *reply) {
 absl::Status AclValkeyCheckPermissions(
     ValkeyModuleCtx *ctx, acl::KeyAccess access,
     const std::vector<std::string> &module_prefixes) {
-  if (!IsKeyPrefixCheckSupported()) {
-    return absl::UnimplementedError(
-        "ValkeyModule_ACLCheckKeyPrefixPermissions is not supported for this "
-        "version of Valkey");
-  }
+  CHECK(IsKeyPrefixCheckSupported());
 
   auto flags = static_cast<unsigned int>(access);
   auto valkey_username =
@@ -389,6 +385,9 @@ absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx,
 
 absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
                             const data_model::IndexSchema &index_schema_proto) {
+  if (!vmsdk::IsRealUserClient(ctx)) {
+    return absl::OkStatus();
+  }
   std::vector<std::string> module_prefixes;
   module_prefixes.reserve(index_schema_proto.subscribed_key_prefixes_size());
   for (const auto &prefix : index_schema_proto.subscribed_key_prefixes()) {
@@ -399,7 +398,7 @@ absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
 
 absl::Status AclPrefixCheck(ValkeyModuleCtx *ctx, acl::KeyAccess access,
                             const std::vector<std::string> &module_prefixes) {
-  if (IsKeyPrefixCheckSupported()) {
+  if (IsKeyPrefixCheckSupported() && vmsdk::IsRealUserClient(ctx)) {
     return AclValkeyCheckPermissions(ctx, access, module_prefixes);
   }
   return AclPrefixCheck(ctx, KeyAccessToStringView(access), module_prefixes);